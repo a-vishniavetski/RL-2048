0d4d23ef58ef1298394e775c2f9205ac86468a3a
a-vishniavetski <a.vishniavetski@gmail.com>
2026-07-17 16:33:53 +0200

    feat: refactor of env

--- play.py
+++ play.py
@@ -11,12 +11,10 @@ from engine import (
 )
 
 
-def evaluate():
-    return random.choice([Move.LEFT, Move.RIGHT, Move.UP, Move.DOWN])
-
-
-def make_move(move: Move, board):
-    move_function = move_left_or_right if move in (Move.LEFT, Move.RIGHT) else move_up_or_down
+def make_move(move: Move, board, rng):
+    move_function = (
+        move_left_or_right if move in (Move.LEFT, Move.RIGHT) else move_up_or_down
+    )
 
     reward, board_has_changed = move_function(board, move)  # board is in afterstate s'
     afterstate = board.copy()
@@ -25,60 +23,59 @@ def make_move(move: Move, board):
     return reward, afterstate
 
 
+def evaluate():
+    return random.choice([Move.LEFT, Move.RIGHT, Move.UP, Move.DOWN])
+
+
 def learn_evaluation(state, action, reward, afterstate, new_state):
     pass
 
-class Env():
+
+class Env:
     pass
 
 
+def play_episode(evaluation_function, evaluation_learning_function):
+    rng = np.random.default_rng(0)
+    history: list[tuple[Move, np.ndarray]] = []
+    state = init_board(rng)
+    score = 0
+    terminal_state = False
+    history.append((None, state.copy()))
+    training = False
+    while not terminal_state:
+        old_state = state.copy()
+        action = evaluation_function(state)
+        reward, afterstate = make_move(action, state, rng)  # board is in new state s''
+
+        if training:
+            evaluation_learning_function(old_state, action, reward, afterstate, state)
+
+        score += reward
+        history.append((action, state.copy()))
+
+        terminal_state = has_lost(state)
+
+    highest_tile = state.max()
+    return score, highest_tile
+
+
 if __name__ == "__main__":
-    number_of_games = 1000
+    number_of_games = 2000
     games_played = 0
-    reward_all_games = 0
+    score_all_games = 0
     highest_tile = 2
 
     while games_played < number_of_games:
-        rng = np.random.default_rng(0)
-        history: list[tuple[Move, np.ndarray]] = []
-        state = init_board(rng)
-        episode_reward = 0
-        terminal_state = False
-        history.append((None, state.copy()))
-        training = False
-        while not terminal_state:
-            old_state = state.copy()
-            action = evaluate()
-            reward, afterstate = make_move(action, state)  # board is in new state s''
-
-            if training:
-                learn_evaluation(old_state, action, reward, afterstate, state)
-
-            # print("\n")
-            # print("State s:")
-            # print(old_state)
-            # print(f"Action: {action.name}")
-            # print(f"Afterstate s':")
-            # print(afterstate)
-            # print(f"New state s'':")
-            # print(state)
-
-            episode_reward += reward
-            history.append((action, state.copy()))
-
-            terminal_state = has_lost(state)
-            # if terminal_state:
-            #     print(f"GAME OVER; Total reward: {episode_reward}")
-
+        episode_score, this_game_highest_tile = play_episode(
+            evaluation_function=evaluate, evaluation_learning_function=learn_evaluation
+        )
         games_played += 1
-        reward_all_games += episode_reward
-        this_game_highest_tile = state.max()
+        score_all_games += episode_score
         if this_game_highest_tile > highest_tile:
             highest_tile = this_game_highest_tile
 
-        _average =reward_all_games / games_played
-        print(f"Played {games_played} games, total reward: {reward_all_games}, Avg.: {_average:.2f}, Highest tile: {highest_tile}")
-
-        # for last_action, _state in history:
-        #     print(f"Last action: {last_action}")
-        #     print(_state, end="\n\n")
+    _average = score_all_games / games_played
+    print(
+        f"Played {games_played} games, Avg. Reward: {_average:.2f}, Highest tile: {highest_tile}"
+    )
