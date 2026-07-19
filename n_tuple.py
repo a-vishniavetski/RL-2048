3fedf40278a256d1dcba33dcdb7d61e498eca326
a-vishniavetski <a.vishniavetski@gmail.com>
2026-07-19 18:03:36 +0200

    feat: implemented the state value function using n_tuple networks

--- n_tuple.py
+++ n_tuple.py
@@ -1,6 +1,9 @@
+from dataclasses import dataclass
+from typing import Tuple, List
 import numpy as np
 
-board = np.array(
+C = 15  # := maximum_power_of_two_allowed - 1
+BOARD = np.array(
     [
         [2, 2, 4, 4],
         [2, 0, 4, 4],
@@ -9,33 +12,47 @@ board = np.array(
     ]
 )
 
-_tuple = np.array([(0, 1), (1, 1), (2, 1), (3, 1)])
+class N_tuple():
+    def __init__(self, locations: List[Tuple[int, int]], c: int = C, tuple_length: int = 4):
+        self.locations = locations  # predetermined sequence of board locations
+        rng = np.random.default_rng()
+        self.lookup_size = C**tuple_length
+        self.lookup_table = np.random.rand(self.lookup_size)
 
-for _row, _col in _tuple:
-    print(board[_row][_col])
+def weight_lookup_index(board_elements: np.ndarray, c: int = C, n: int = 4):
+    # we encode board values ("2", "16") into their powers of 2 (1, 4); then convert to base-c from base-10
 
-# all_combinations =
-# _tuple_weights = np.array(  # LUT mapping all possible combinations of 4 consequtive powers of 2 up to 2**15; need to encode board values with log;
+    assert isinstance(board_elements, np.ndarray), "board values must be a NumPy ndarray"
 
-# )
-
-powers_of_two = [0,] + [2**_pow for _pow in range(1, 15)]
-board_states = [(i, j) for j in powers_of_two for i in powers_of_two]
-# print(board_states)
-
-
-def lut_index(n_tuple: np.ndarray, c: int = 15, n: int = 2):
-    assert isinstance(n_tuple, np.ndarray), "n_tuple must be a NumPy ndarray"
-
-    encoded_tuple = np.zeros_like(n_tuple)
-    mask = n_tuple != 0
-    encoded_tuple[mask] = np.log2(n_tuple[mask]).astype(np.int32)
-    _index = 0
+    encoded_board_elements = np.zeros_like(board_elements)
+    non_zero_mask = board_elements != 0
+    encoded_board_elements[non_zero_mask] = np.log2(board_elements[non_zero_mask]).astype(np.int32)
+    weight_index = 0
     for tuple_element_position in range(0, n):
-        value = encoded_tuple[tuple_element_position]
-        _index += value * (c ** (tuple_element_position))
-    return _index
+        value = encoded_board_elements[tuple_element_position]
+        weight_index += value * (c ** (tuple_element_position))
+    return weight_index
+
+# n-tuple network of m tuples implements a function approximator f(s) :=
+def state_value_function(tuple_network: List[N_tuple], board_state: np.ndarray):
+    state_value = 0
+    for _tuple in tuple_network:
+        rows, cols = zip(*_tuple.locations)
+        relevant_elements = board_state[rows, cols]
+        combination_weight = _tuple.lookup_table[weight_lookup_index(relevant_elements)]
+        state_value += combination_weight
+    return state_value
+
+if __name__ == "__main__":
+    n_tuple = N_tuple([(0,1), (1, 1), (2, 1), (3, 1)])
+    print("Tuple locations:", n_tuple.locations)
+    rows, cols = zip(*n_tuple.locations)
+    relevant_elements = BOARD[rows, cols]
+    print("Relevant elements", relevant_elements)
+    print(f"Index of relevant elements: {weight_lookup_index(relevant_elements)}")
+    print(len(n_tuple.lookup_table))  # at C = 15 and n = 4 matches the paper network 17* 15*4 = 860 625
+    print(f"Weight at the index: {n_tuple.lookup_table[weight_lookup_index(relevant_elements)]}")
+    board_value = state_value_function([n_tuple,], BOARD)
+    print(board_value)
 
 
-for _state in board_states[:17]:
-    print(_state, lut_index(np.asarray(_state)))
